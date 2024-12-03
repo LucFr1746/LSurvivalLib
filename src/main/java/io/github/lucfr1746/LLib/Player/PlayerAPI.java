8660e8d6d48cc049dc6b86aec9027368c81f6ec8
Thế Lực <LucFr.1746@gmail.com>
2024-12-03 15:03:35 +0700

    Added more API to PLayer

--- src/main/java/io/github/lucfr1746/LLib/Player/PlayerAPI.java
+++ src/main/java/io/github/lucfr1746/LLib/Player/PlayerAPI.java
@@ -1,10 +1,14 @@
 package io.github.lucfr1746.LLib.Player;
 
 import io.github.lucfr1746.LLib.Utils.TextAPI;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 public class PlayerAPI {
 
@@ -16,6 +20,10 @@ public class PlayerAPI {
 
     public PlayerAPI(HumanEntity humanEntity) {
         this.player = (Player) humanEntity;
+    }
+
+    public Player getPlayer() {
+        return this.player;
     }
 
     public void addItem(ItemStack itemStack) {
@@ -45,5 +53,41 @@ public class PlayerAPI {
     public PlayerAPI sendChatBorder(String customColor) {
         sendColoredMessage(customColor + "▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬");
         return this;
+    }
+
+    public PlayerAPI sendSuccessMessage(String message) {
+        sendColoredMessage(ChatColor.GREEN + message);
+        return this;
+    }
+
+    public PlayerAPI sendWarningMessage(String message) {
+        sendColoredMessage(ChatColor.YELLOW + message);
+        return this;
+    }
+
+    public PlayerAPI sendErrorMessage(String message) {
+        sendColoredMessage(ChatColor.RED + message);
+        return this;
+    }
+
+    public PlayerAPI playSoundAtPlayerLoc(Sound sound) {
+        this.player.playSound(this.player.getLocation(), sound, 1f, 1f);
+        return this;
+    }
+
+    public PlayerAPI playButtonClickSound() {
+        playSoundAtPlayerLoc(Sound.UI_BUTTON_CLICK);
+        return this;
+    }
+
+    public PlayerAPI openInventory(Plugin plugin, Inventory inventory) {
+        Bukkit.getScheduler().runTask(plugin, ()
+                -> this.player.openInventory(inventory));
+        return this;
+    }
+
+    public PlayerAPI openInventory(Plugin plugin, Inventory inventory, Sound openSound) {
+        playSoundAtPlayerLoc(openSound);
+        return openInventory(plugin, inventory);
     }
 }
