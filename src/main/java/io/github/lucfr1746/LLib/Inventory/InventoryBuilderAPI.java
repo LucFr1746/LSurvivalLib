d036ee188ed94c44116eb7f041256602570f967f
Thế Lực <LucFr.1746@gmail.com>
2024-12-06 20:31:49 +0700

    Added Lock Mode to InventoryBuilderAPI

--- src/main/java/io/github/lucfr1746/LLib/Inventory/InventoryBuilderAPI.java
+++ src/main/java/io/github/lucfr1746/LLib/Inventory/InventoryBuilderAPI.java
@@ -6,14 +6,21 @@ import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.PlayerInventory;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public abstract class InventoryBuilderAPI implements InventoryHandler {
 
+    public enum LockMode {
+        ALL, // Prevents all interaction
+        GUI_LOCKED   // Prevents interaction with the GUI
+    }
+
     private final Inventory inventory;
     private final Map<Integer, InventoryButton> buttonMap = new HashMap<>();
+    private LockMode lockMode = LockMode.ALL;
 
     public InventoryBuilderAPI() {
         this.inventory = this.createInventory();
@@ -27,6 +34,14 @@ public abstract class InventoryBuilderAPI implements InventoryHandler {
         this.buttonMap.put(slot, button);
     }
 
+    public void setLockMode(LockMode lockMode) {
+        this.lockMode = lockMode;
+    }
+
+    public LockMode getLockMode() {
+        return this.lockMode;
+    }
+
     public void decorate(Player player) {
         this.buttonMap.forEach((slot, button) -> {
             ItemStack icon = button.getIconCreator().apply(player);
@@ -36,9 +51,23 @@ public abstract class InventoryBuilderAPI implements InventoryHandler {
 
     @Override
     public void onClick(InventoryClickEvent event) {
+        if (event.getClickedInventory() == null) return;
         event.setCancelled(true);
         int slot = event.getSlot();
         InventoryButton button = this.buttonMap.get(slot);
+
+        switch (lockMode) {
+            case ALL:
+                event.setCancelled(true);
+                break;
+            case GUI_LOCKED:
+                if (event.getClickedInventory() instanceof PlayerInventory) {
+                    return;
+                }
+                event.setCancelled(true);
+                break;
+        }
+
         if (button != null) {
             button.getEventConsumer().accept(event);
         }
